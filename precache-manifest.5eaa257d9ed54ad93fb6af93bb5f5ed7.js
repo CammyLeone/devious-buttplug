593abb8bd370a691bebe6e377c3a3dc7f57e3722
cammyleone <cammy@willyouplease.me>
2020-09-19 11:42:12 -0700

    Adding built assets for deploy

---
+++ precache-manifest.5eaa257d9ed54ad93fb6af93bb5f5ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2721bab42402444a4cd4682a65aa15d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "973b509dcc827e18f872",
+    "url": "/static/css/main.60b73a3f.chunk.css"
+  },
+  {
+    "revision": "4fec7a5c7582f78f39de",
+    "url": "/static/js/2.1cd002b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1cd002b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973b509dcc827e18f872",
+    "url": "/static/js/main.9415689c.chunk.js"
+  },
+  {
+    "revision": "344f7c7ff63669f887cb",
+    "url": "/static/js/runtime-main.7c5c2f62.js"
+  }
+]);
